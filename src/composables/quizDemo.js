818c9abf50a8bd85c159160b1b3ce8eb53056cde
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-10 22:26:48 +0200

    Reformat code

--- src/composables/quizDemo.js
+++ src/composables/quizDemo.js
@@ -1,25 +1,22 @@
 import { ref, onMounted } from "vue";
-import {
-    fetchQuiz,
-    storeQuiz
-} from "../apis/quiz.js";
+import { fetchQuiz, storeQuiz } from "../apis/quiz.js";
 
 export default function useQuiz() {
-    const getQuiz = ref([]);
+  const getQuiz = ref([]);
 
-    const setQuiz = async () => {
-        getQuiz.value = await fetchQuiz()
-    };
+  const setQuiz = async () => {
+    getQuiz.value = await fetchQuiz();
+  };
 
-    const createQuiz = (question) => {
-        storeQuiz(question);
-    };
+  const createQuiz = (question) => {
+    storeQuiz(question);
+  };
 
-    onMounted(setQuiz);
+  onMounted(setQuiz);
 
-    return {
-        getQuiz,
-        createQuiz,
-        setQuiz
-    };
+  return {
+    getQuiz,
+    createQuiz,
+    setQuiz,
+  };
 }
