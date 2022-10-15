798781529d3d2d4fc1964020329a0c8ec2bb350d
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-15 17:06:01 +0200

    Change about to profile

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import QuizDemo from "../views/QuizDemoView.vue";
-import QuizOptions from "../views/QuizOptionsView.vue"
-import HomeView from "../views/HomeView.vue"
+import QuizOptions from "../views/QuizOptionsView.vue";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,8 +22,8 @@ const router = createRouter({
       component: QuizDemo,
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/profile",
+      name: "profile",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
