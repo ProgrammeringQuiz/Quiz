8128690713416e449d7789d98afd0337c8f3ed47
Ahsanadam <adamebadul@gmail.com>
2022-10-20 10:18:28 +0200

    Refactor Color change when user choose alternative, total score , next and previous functions

--- src/stores/quiz.js
+++ src/stores/quiz.js
@@ -1,23 +1,42 @@
 
 import {defineStore} from "pinia";
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 
 export const useQuizStore = defineStore('quiz', () => {
     const questionCompleted = ref(false)
     const questionNumber = ref(0)
-    const quiz = ref()
+    const quiz = ref([])
+    const quizCompleted = ref(false)
+    const userAnswers = ref ([])
+
+    const totalScore = computed(() => {
+        let score = 0
+        quiz.value.forEach((question, index) => {
+            if(question.answer === userAnswers.value[index]){
+
+                score++
+            }
+        })
+        return score
+    })
+
+    function addAnswer(answer){
+        userAnswers.value.push(answer)
+
+    }
 
     function nextQuestion() {
         if (questionNumber.value < quiz.value.length - 1) {
             questionNumber.value++
-        } else {
-            questionCompleted.value = true
+        }else {
+            quizCompleted.value = true
+
         }
+
     }
     function setQuiz(quizData){
 
         quiz.value = quizData
-
     }
 
     function previousQuestion() {
@@ -33,6 +52,7 @@ export const useQuizStore = defineStore('quiz', () => {
 
     } )
 
-    return {questionCompleted, questionNumber, nextQuestion,previousQuestion, quiz, setQuiz}
+    return {questionCompleted, questionNumber, nextQuestion,previousQuestion, quiz, setQuiz, quizCompleted, addAnswer, userAnswers,
+    totalScore}
 
 })
