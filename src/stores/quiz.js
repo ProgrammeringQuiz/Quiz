e11eaecd5314ade99e187f48e5644703d5da9add
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-23 15:12:01 +0800

    Add connection to mongodb

--- src/stores/quiz.js
+++ src/stores/quiz.js
@@ -1,58 +1,61 @@
+import { defineStore } from "pinia";
+import { computed, onMounted, ref } from "vue";
 
-import {defineStore} from "pinia";
-import {computed, onMounted, ref} from "vue";
+export const useQuizStore = defineStore("quiz", () => {
+  const questionCompleted = ref(false);
+  const questionNumber = ref(0);
+  const quiz = ref([]);
+  const quizCompleted = ref(false);
+  const userAnswers = ref([]);
 
-export const useQuizStore = defineStore('quiz', () => {
-    const questionCompleted = ref(false)
-    const questionNumber = ref(0)
-    const quiz = ref([])
-    const quizCompleted = ref(false)
-    const userAnswers = ref ([])
+  const totalScore = computed(() => {
+    let score = 0;
+    quiz.value.forEach((question, index) => {
+      if (question.answer === userAnswers.value[index]) {
+        score++;
+      }
+    });
+    return score;
+  });
 
-    const totalScore = computed(() => {
-        let score = 0
-        quiz.value.forEach((question, index) => {
-            if(question.answer === userAnswers.value[index]){
+  function addAnswer(answer) {
+    userAnswers.value.push(answer);
+  }
 
-                score++
-            }
-        })
-        return score
-    })
-
-    function addAnswer(answer){
-        userAnswers.value.push(answer)
-
+  function nextQuestion() {
+    if (questionNumber.value < quiz.value.length - 1) {
+      questionNumber.value++;
+    } else {
+      quizCompleted.value = true;
     }
+  }
+  function setQuiz(quizData) {
+    quiz.value = quizData;
+  }
 
-    function nextQuestion() {
-        if (questionNumber.value < quiz.value.length - 1) {
-            questionNumber.value++
-        }else {
-            quizCompleted.value = true
+  function previousQuestion() {
+    if (questionNumber.value > 0) questionNumber.value--;
+  }
 
-        }
+  onMounted(() => {
+    fetch("http://localhost:8080/api/quiz")
+      .then((response) => response.json())
+      .then((data) => {
+        quiz.value = data.data;
+        console.log(data);
+      });
+  });
 
-    }
-    function setQuiz(quizData){
-
-        quiz.value = quizData
-    }
-
-    function previousQuestion() {
-        if (questionNumber.value > 0) questionNumber.value--
-    }
-
-    onMounted(() => {
-        fetch('http://localhost:8080/getQuestions').then(response => response.json()).then(data => {
-            quiz.value = data.quizDemo
-            console.log(data)
-
-        })
-
-    } )
-
-    return {questionCompleted, questionNumber, nextQuestion,previousQuestion, quiz, setQuiz, quizCompleted, addAnswer, userAnswers,
-    totalScore}
-
-})
+  return {
+    questionCompleted,
+    questionNumber,
+    nextQuestion,
+    previousQuestion,
+    quiz,
+    setQuiz,
+    quizCompleted,
+    addAnswer,
+    userAnswers,
+    totalScore,
+  };
+});
