fd270faf9a2a63f4bc197e393b7f953cb0364d69
Lukas Finne <lukas.ikvm1@gmail.com>
2022-11-07 10:53:29 +0100

    Fix CSS for sign-up

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,8 +1,8 @@
 <script setup>
-import {signUpRequest} from "@/apis/signUp";
-import {ref} from "vue";
-import {useAuthStore} from "@/stores/authStore.js";
-import FileInput from 'vue3-simple-file-input'
+import { signUpRequest } from "@/apis/signUp";
+import { ref } from "vue";
+import { useAuthStore } from "@/stores/authStore.js";
+import FileInput from "vue3-simple-file-input";
 
 let authStore = useAuthStore();
 
@@ -14,59 +14,82 @@ const email = ref();
 const profileImage = ref();
 
 const signUp = async () => {
-  console.log("profileImage: ", profileImage.value)
+  console.log("profileImage: ", profileImage.value);
   await authStore.signUp({
     username: `${username.value}`,
     password: `${password.value}`,
     fName: `${firstName.value}`,
     lName: `${lastName.value}`,
     email: `${email.value}`,
-    profileImage: `${profileImage.value.fileBlob.toString()}`
-  })
-
-}
-
+    profileImage: `${profileImage.value.fileBlob.toString()}`,
+  });
+};
 </script>
 
 <template>
   <form action="" @submit.prevent="onSubmit">
     <h1>Sign up</h1>
     <label for="username"></label>
-    <input type="text" placeholder="Username" id="username" v-model="username"/>
+    <input
+      class="userInput"
+      type="text"
+      placeholder="Username"
+      id="username"
+      v-model="username"
+    />
     <label for="password"></label>
-    <input type="password" placeholder="Password" id="password" v-model="password"/>
+    <input
+      class="userInput"
+      type="password"
+      placeholder="Password"
+      id="password"
+      v-model="password"
+    />
     <label for="firstName"></label>
-    <input type="text" placeholder="First Name" id="firstName" v-model="firstName"/>
+    <input
+      class="userInput"
+      type="text"
+      placeholder="First Name"
+      id="firstName"
+      v-model="firstName"
+    />
     <label for="lastName"></label>
-    <input type="text" placeholder="Last Name" id="lastName" v-model="lastName"/>
+    <input
+      class="userInput"
+      type="text"
+      placeholder="Last Name"
+      id="lastName"
+      v-model="lastName"
+    />
     <label for="email"></label>
-    <input type="text" placeholder="Email" id="email" v-model="email"/>
-    <label for="file"></label>
-    <file-input v-model="profileImage" />
+    <input
+      class="userInput"
+      type="text"
+      placeholder="Email"
+      id="email"
+      v-model="email"
+    />
+
+    <label class="fileLabel">
+      Choose a file:
+      <input type="file" name="avatar" />
+    </label>
 
     <button @click="signUp()">Confirm</button>
-
-
-
-
   </form>
 </template>
 
 <style scoped>
-
 form {
-  height: 500px;
-  width: 500px;
+  display: flex;
+  flex-direction: column;
   background-color: #2d4263;
-  position: absolute;
-  transform: translate(-50%, -50%);
-  top: 50%;
-  left: 50%;
-  border-radius: 10px;
-  backdrop-filter: blur(10px);
+  max-width: 20em;
+  margin: 10em auto;
+  border-radius: 0.625em;
   border: 2px solid rgba(255, 255, 255, 0.1);
   box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
-  padding: 50px 35px;
+  padding: 3.125em 2.1875em;
 }
 
 form * {
@@ -74,6 +97,7 @@ form * {
   color: #ffffff;
   outline: none;
   border: none;
+  justify-content: center;
 }
 
 form h1 {
@@ -89,11 +113,13 @@ label {
   font-weight: 500;
 }
 
+.userInput {
+  background-color: rgba(255, 255, 255, 0.07);
+}
+
 input {
   display: block;
   height: 50px;
-  width: 96%;
-  background-color: rgba(255, 255, 255, 0.07);
   border-radius: 3px;
   padding: 0 10px;
   margin-top: 8px;
@@ -109,7 +135,6 @@ input {
 button {
   margin-top: 30px;
   margin-bottom: 30px;
-  width: 100%;
   background-color: rgba(255, 255, 255, 0.07);
   color: #e3e3e3;
   padding: 15px 0;
@@ -122,7 +147,15 @@ button {
 button:hover {
   background-color: #232747;
 }
-
-
-
+input[type="file"] {
+  display: block;
+  margin: 0;
+  padding: 0;
+}
+.fileLabel {
+  margin-top: 0.5em;
+  padding: 0.5em;
+  background-color: rgba(255, 255, 255, 0.07);
+  cursor: pointer;
+}
 </style>
